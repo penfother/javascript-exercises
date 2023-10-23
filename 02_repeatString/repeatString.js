2157c4184add924fa39c3fd30b75f2bd86fe0ca1
penfother <filip.pendic2@gmail.com>
2023-10-23 17:21:59 +0200

    added for loop, and if clause in case a decided number is of negative value that returns ERROR

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(string, num) {
+    let newString = "";
+    if (num < 0){
+        return "ERROR";
+    } else { 
+        for (let i = 0; i < num; i++) {
+        newString = newString + string;
+        }
+    return newString;}
 };
 
 // Do not edit below this line
