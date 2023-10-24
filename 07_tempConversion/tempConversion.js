5af349a2e1c6aa12a57291e6892df788435238f2
penfother <filip.pendic2@gmail.com>
2023-10-24 23:01:18 +0200

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  
+  // calculates according to formula
+  convertedFahrenheit = (fahrenheit - 32) * (5/9);
+
+  //rounds the output
+  convertedFahrenheit = (Math.round(convertedFahrenheit * 10)) / 10;
+  
+  return convertedFahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  // calculates according to formula
+  convertedCelsius = celsius * (9/5) + 32;
+
+  // rounds the output
+  convertedCelsius = (Math.round(convertedCelsius * 10)) / 10;
+
+  return convertedCelsius;
 };
 
 // Do not edit below this line
