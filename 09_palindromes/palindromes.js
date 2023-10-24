808a65b7690ed953bab151ada473882b756455ae
penfother <filip.pendic2@gmail.com>
2023-10-25 01:38:00 +0200

    passed all tests, added space removal for starting comparison

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,21 @@
 const palindromes = function (testPalindromString) {
     
-    //removes punctuation, uppercase letters, numbers
+    //removes punctuation, uppercase letters, numbers from starting string
     let noShitString = testPalindromString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()1234567890]/g,"").toLowerCase();
 
     let backwardsArray = [];
+    let noShitStringArray = [];
+
+    //checks if the tested string has any spaces, if there are replaces them with a zero then removes them
+    for (i = 0; i < noShitString.length; i++) {
+        noShitStringArray[i] = noShitString.slice(i, i+1);
+        if (noShitStringArray[i] == " ") {
+            noShitStringArray[i] = "0";
+        };         
+    };
+
+    noShitString = noShitStringArray.join("").replace(/[0]/g,"");
+    
     let backPointer = noShitString.length;
 
     //puts pieces of string into array in a backwards order
@@ -11,8 +23,6 @@ const palindromes = function (testPalindromString) {
         backPointer -= 1;
         backwardsArray[i] = noShitString.slice(backPointer, backPointer + 1);
     };
-
-    //removes spaces from backwardsArray
 
     //assembles backwardsArray into string WITHOUT SPACES INBETWEEN WORDS
     testArray = backwardsArray.join("");
