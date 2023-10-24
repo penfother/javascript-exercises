e149269f2cdea2dcb64e22f7eea6ee641dea376f
penfother <filip.pendic2@gmail.com>
2023-10-24 23:37:25 +0200

    done adding, substraction, sum array, and multiplication array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,35 @@
-const add = function() {
-	
+const add = function(additor1, additor2) {
+  plus = additor1 + additor2;
+  return plus;	
 };
 
-const subtract = function() {
-	
+const subtract = function(subtractor1, subtractor2) {
+	minus = subtractor1 - subtractor2;
+  return minus;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	sumsValue = 0;
+  for (i = 0; i < sumArray.length; i++) {
+    sumsValue += sumArray[i];  
+  };
+  return sumsValue;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  firstTwo = multiplyArray[0] * multiplyArray[1];
+  multiplyArray.shift();
+  multiplyArray.shift();
+  if (multiplyArray.length == 0){
+    return firstTwo;
+  } else {
+    let multiplicate = 1;
+    for (i = 0; i < multiplyArray.length; i++){
+      multiplicate *= multiplyArray[i];
+    };
+    multiplicate *= firstTwo; 
+    return multiplicate;
+  };
 };
 
 const power = function() {
