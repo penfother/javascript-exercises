e104be8a9e9a8ddaf5cb4b56b8f8c1afa1c40e37
penfother <filip.pendic2@gmail.com>
2023-10-24 21:42:01 +0200

    first test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let startingNum = startNum;
+    let endingNum = endNum;
+//    if (startNum < 0 || endNum < 0) {
+//        return "ERROR";
+//    } else {
+//        if (endNum < startNum) {
+//            startingNum = endNum;
+//            endingNum = startNum;
+//        }
+        let sum = startingNum;
+        let counterNum = startingNum;
+        for (let i = 0; i < endingNum - startingNum; i++) {
+            counterNum += 1;
+            sum += counterNum;
+//        };
+    };
+    return sum;
 };
 
 // Do not edit below this line
