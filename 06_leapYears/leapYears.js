374ecc47cc0d125b8df2fe17314b11c54409b11f
penfother <filip.pendic2@gmail.com>
2023-10-24 22:45:05 +0200

    passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    let test = false;
 
+    //tests if the year can even be considered for a leap year
+    if (testYear % 4 == 0) {
+        test = true;
+    };
+
+    //tests if the year is divisible by 100
+    if (testYear % 100 == 0) {
+        test = false;
+    };
+
+    //tests if the year is divisible by 100 and 400
+    if (testYear % 100 == 0 && testYear % 400 == 0) {
+        test = true;
+    };
+
+    return test;
 };
 
 // Do not edit below this line
