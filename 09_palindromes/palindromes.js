9b342ad012cae6f0ded18935d25dc3d6641f1248
penfother <filip.pendic2@gmail.com>
2023-10-25 00:37:40 +0200

    passed first two tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (testPalindromString) {
+    
+    //removes punctuation, uppercase letters
+    let noShitString = testPalindromString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    noShitString = noShitString.toLowerCase();
 
+
+    let backwardsArray = [];
+    let backPointer = noShitString.length;
+
+    //puts pieces of string into array in a backwards order
+    for (i = 0; i < noShitString.length; i++) {
+        backPointer -= 1;
+        backwardsArray[i] = noShitString.slice(backPointer, backPointer + 1);
+    };
+
+    //assembles backwardsarray into string
+    testArray = backwardsArray.join("");
+
+    //removes punctuation 
+
+
+    //tests if the starting string is the same as the newly assembled one
+    if (testArray == noShitString) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
