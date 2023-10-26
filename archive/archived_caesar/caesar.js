dc0a3314715e9d96236650d748223c6fc15fa891
penfother <filip.pendic2@gmail.com>
2023-10-26 14:47:55 +0200

    cyphered phrase has correctly assigned upper and lower case letters separated in an array

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,6 +1,5 @@
 const caesar = function(letters, num) {
 //creates letters of the alphabet in an array
-    letters = letters.toUpperCase();
 
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 
@@ -14,9 +13,11 @@ const caesar = function(letters, num) {
 
 //takes the input letters and disassociates them into an array
     let lettersArray = [];
-    
+    let upperLowerArray = [];
+
     for (let i = 0; i < letters.length; i++) {
-        lettersArray[i] = letters.slice(i, i + 1);
+        lettersArray[i] = letters.toUpperCase().slice(i, i + 1);
+        upperLowerArray[i] = letters.slice(i, i + 1);
     };
 
 //changes the input  letters for their equivalent in cypher array
@@ -30,8 +31,19 @@ const caesar = function(letters, num) {
         };
     };
 
+//checks for uppercase and lowercase letters in input phrase
+    for (let i = 0; i < upperLowerArray.length; i++) {
+        if (upperLowerArray[i] == upperLowerArray[i].toLowerCase()) {
+            cypher[i] = cypher[i].toLowerCase();
+        };
+    };
+
+
+//returns finished cypher
+    return cypher;
+
     //added for debugging
-    return lettersArray;
+    //return letters;
 };
 
 // Do not edit below this line
